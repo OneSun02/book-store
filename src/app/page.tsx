e67cb23581ab36b5547619d8f9bb188a4f6590b8
Pham Xuan Nhat <161459493+OneSun02@users.noreply.github.com>
2025-08-29 10:55:32 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import ProductList from "@/components/ProductList";
 
 const prisma = new PrismaClient();
 
+// Force HomePage luôn dynamic, fetch data mỗi lần request
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const products = await prisma.product.findMany({ include: { images: true } });
   const categoriesData = await prisma.product.findMany({

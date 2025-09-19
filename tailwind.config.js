c0013a221f26cf70145e3a3141791a9ec280b2a3
OneSun02 <nhat280602@gmail.com>
2025-09-20 06:15:26 +0700

    fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+})

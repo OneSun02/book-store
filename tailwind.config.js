934047ad2641d13b17fd1119fcac24a8ce03894a
OneSun02 <nhat280602@gmail.com>
2025-09-20 05:54:16 +0700

    fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: {
-    tailwindcss,
-    autoprefixer,
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [], // thêm plugin như forms nếu muốn
 };
